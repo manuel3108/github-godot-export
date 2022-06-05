b22cbe60625730784ba9bcce6683088aefd19c76
Manuel Serret <mserret99@gmail.com>
2022-06-05 22:23:31 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,25 +1,67 @@
 const core = require('@actions/core');
 const { Octokit } = require('@octokit/core');
+const fs = require('fs');
+const http = require('http');
 
 // most @actions toolkit packages have async methods
 async function run() {
     try {
         const godotVersion = core.getInput('godot_version');
+        const useMono = core.getInput('use_mono');
+
+        core.info(`Godot version: ${godotVersion}`);
+        core.info(`Use Mono: ${useMono}`);
+
+        core.info();
+        core.info('Getting the release for the selected version...');
 
         const octokit = new Octokit();
-        const release = await octokit.request(
-            'GET /repos/{owner}/{repo}/releases/tags/{tag}',
-            {
-                owner: 'godotengine',
-                repo: 'godot',
-                tag: godotVersion,
-            }
-        );
+        const release = await octokit.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
+            owner: 'godotengine',
+            repo: 'godot',
+            tag: godotVersion,
+        });
 
-        core.info(JSON.stringify(release.data.assets));
+        core.info(`Found release with id ${release.data.id} created at ${release.data.created_at}`);
+
+        const assets = release.data.assets;
+        const headlessGodotAsset = assets.find((asset) => asset.name === `Godot_v${godotVersion}${useMono ? '_mono' : ''}_linux_headless_64.zip`);
+        const exportTemplatesAsset = assets.find((asset) => asset.name === `Godot_v${godotVersion}${useMono ? '_mono' : ''}_export_templates.tpz`);
+
+        core.info(`Found headless Godot asset and downloading it from ${headlessGodotAsset.browser_download_url}`);
+        core.info(`Found export templates asset and downloading it from ${exportTemplatesAsset.browser_download_url}`);
+
+        const downloadGodot = downloadFile(headlessGodotAsset.browser_download_url, headlessGodotAsset.name);
+        const downloadExportTemplates = downloadFile(exportTemplatesAsset.browser_download_url, exportTemplatesAsset.name);
+        await Promise.all([downloadGodot, downloadExportTemplates]);
+
+        core.info('Finished downloading the files!');
     } catch (error) {
         core.setFailed(error.message);
     }
 }
 
-run();
+function getFile(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, (response) => {
+            const { statusCode } = response;
+            if (statusCode === 200) {
+                resolve(response);
+            }
+            reject(null);
+        });
+    });
+}
+
+async function downloadFile(url, fileName) {
+    const response = await getFile(url);
+    if (response) {
+        const file = fs.createWriteStream(fileName);
+        response.pipe(file);
+    }
+}
+
+run().catch((err) => {
+    core.setFailed(err.message);
+    process.exit(1);
+});
