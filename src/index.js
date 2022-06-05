f3b836e4e1899896a8e13d49acb52a7ffea8c8c9
Manuel Serret <mserret99@gmail.com>
2022-06-05 21:56:14 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const core = require('@actions/core');
+const { Octokit } = require('@octokit/core');
 
 // most @actions toolkit packages have async methods
 async function run() {
     try {
-        const name = core.getInput('godot_version');
-        console.log(`Hello ${name}!`);
+        const godotVersion = core.getInput('godot_version');
+
+        const octokit = new Octokit();
+        const release = await octokit.request(
+            'GET /repos/{owner}/{repo}/releases/tags/{tag}',
+            {
+                owner: 'godotengine',
+                repo: 'godot',
+                tag: godotVersion,
+            }
+        );
+
+        core.info(JSON.stringify(release.data.assets));
     } catch (error) {
         core.setFailed(error.message);
     }
